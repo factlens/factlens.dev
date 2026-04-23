798102275fe5487176adeea0e7e6bdaca80f7a74
factlens <javier@factlens.dev>
2026-04-23 18:09:34 +0200

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,7 @@ const {
 
 // https://astro.build/config
 const config = defineConfig({
-	site: 'https://spectre.lou.gg',
+	site: 'https://factlens.dev',
 	output: 'static',
 	integrations: [
 		expressiveCode({
